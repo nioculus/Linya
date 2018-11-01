810f21ebed4d33d59fb2df5ae513d68326c201ce
jeromeestrella <44581878+jeromeestrella@users.noreply.github.com>
2018-11-02 02:22:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -320,8 +320,8 @@ function showDivs(n) {
 			</div>
 		</div>
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="jquery.min.js"></script>
+    <script src="bootstrap.bundle.min.js"></script>
 
     <!-- Plugin JavaScript -->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
